627d5d9d35cfdd9bd37d9059b8366452e69b3ae9
Harshit Singh-Codes <harshitpanu7900@gmail.com>
2023-10-25 22:42:40 +0530

    udating jobs.groovy

--- jobs.groovy
+++ jobs.groovy
@@ -1,15 +1,15 @@
 job('dsl-child-job-01') {
   steps {
-    shell('echo -e "$JOB_NAME is running... \n Buid No.- $BUILD_NUMBER"')
+    shell('echo -e "$JOB_NAME is running... \n Buid No. $BUILD_NUMBER"')
   }
 }
 job('dsl-child-job-02') {
   steps {
-    shell('echo -e "$JOB_NAME is running... \n Buid No.- $BUILD_NUMBER"')
+    shell('echo -e "$JOB_NAME is running... \n Buid No. $BUILD_NUMBER"')
   }
 }
 job('dev-child-job-03') {
   steps {
-    shell('echo -e "$JOB_NAME is running... \n Buid No.- $BUILD_NUMBER"')
+    shell('echo -e "$JOB_NAME is running... \n Buid No. $BUILD_NUMBER"')
   }
 }
