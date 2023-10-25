404607381e433c406ff3953bef077a452b87b445
Harshit Singh-Codes <harshitpanu7900@gmail.com>
2023-10-25 21:57:46 +0530

    adding A-04 grrovy file

---
+++ A-04_jobs.groovy
@@ -0,0 +1,45 @@
+job('dev-01') {
+  steps {
+    shell('echo -e "$JOB_NAME is running... \n Buid No.- $BUILD_NUMBER"')
+  }
+}
+job('dev-02') {
+  steps {
+    shell('echo -e "$JOB_NAME is running... \n Buid No.- $BUILD_NUMBER"')
+  }
+}
+job('dev-03') {
+  steps {
+    shell('echo -e "$JOB_NAME is running... \n Buid No.- $BUILD_NUMBER"')
+  }
+}
+job('test-01') {
+  steps {
+    shell('echo -e "$JOB_NAME is running... \n Buid No.- $BUILD_NUMBER"')
+  }
+}
+job('test-02') {
+  steps {
+    shell('echo -e "$JOB_NAME is running... \n Buid No.- $BUILD_NUMBER"')
+  }
+}
+job('test-03') {
+  steps {
+    shell('echo -e "$JOB_NAME is running... \n Buid No.- $BUILD_NUMBER"')
+  }
+}
+job('devops-01') {
+  steps {
+    shell('echo -e "$JOB_NAME is running... \n Buid No.- $BUILD_NUMBER"')
+  }
+}
+job('devops-02') {
+  steps {
+    shell('echo -e "$JOB_NAME is running... \n Buid No.- $BUILD_NUMBER"')
+  }
+}
+job('devops-03') {
+  steps {
+    shell('echo -e "$JOB_NAME is running... \n Buid No.- $BUILD_NUMBER"')
+  }
+}
